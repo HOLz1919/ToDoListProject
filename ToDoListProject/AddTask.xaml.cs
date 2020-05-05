88fb7a3a1c22b4d9d5f96860e5398926da8090a3
Mateusz Harasimowicz <matirati@gmail.com>
2020-05-05 19:21:27 +0200

    Dodanie wyszukiwania odpowiedniej listy w liście

--- ToDoListProject/AddTask.xaml.cs
+++ ToDoListProject/AddTask.xaml.cs
@@ -41,20 +41,34 @@ namespace ToDoListProject
 
         private void AddSubStepButton_Click(object sender, RoutedEventArgs e)
         {
+
+            
             int index = StepsListBox.SelectedIndex;
 
-
-            if (Steps[index].SubSteps == null)
+            foreach (var listbox in FindVisualChildren<ListBox>(this))
             {
-                ObservableCollection<SubStep> subSteps = new ObservableCollection<SubStep>();
-                subSteps.Add(new SubStep("", false));
-                Steps[index].SubSteps = subSteps;
+                if(listbox.Name== "SubStepsListBox")
+                {
+                    if (Steps[index].SubSteps == null)
+                    {
+                        ObservableCollection<SubStep> subSteps = new ObservableCollection<SubStep>();
+                        subSteps.Add(new SubStep("czesc", false));
+                        Steps[index].SubSteps = subSteps;
+                        listbox.Visibility = Visibility.Visible;
 
+                    }
+                    else
+                    {
+                        Steps[index].SubSteps.Add(new SubStep("dodaje nowe", false));
+                        
+                    }
+                }
             }
-            else
-            {
-                Steps[index].SubSteps.Add(new SubStep("", false));
-            }
+
+            
+
+
+            
         }
 
         private void InitializeCollection()
@@ -65,7 +79,22 @@ namespace ToDoListProject
             }
         }
 
+        public IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
 
+                    if (child != null && child is T)
+                        yield return (T)child;
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                        yield return childOfChild;
+                }
+            }
+        }
 
     }
 }
